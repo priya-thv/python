50155fd43f4c1203825fcf13c007c9a87e98f084
priya <107272239+priya-thv@users.noreply.github.com>
2023-04-27 00:24:28 +0530

    Create ifelse.py

---
+++ sem4/ifelse.py
@@ -0,0 +1,11 @@
+a=int(input("entr the percentage"))
+
+if a>90 and a<100:
+    print("a")
+elif a>80 and a<=90:
+    print("b")
+elif a>70 and a<=80:
+    print("c")
+else:
+    print("fail")
+    
