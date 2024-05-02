b28019572e1304b3348e437f3bc7253db9ac8b86
priya <107272239+priya-thv@users.noreply.github.com>
2024-05-02 21:53:27 +0530

    Create intro.py

---
+++ sem4/intro.py
@@ -0,0 +1,7 @@
+#import numpy as np
+
+print("hello word")
+name=input("Enter a first name: ")
+lname=input("Enter the last name: ")
+age=input("enter your age: ")
+print(name+" "+ lname +" is a genius")
