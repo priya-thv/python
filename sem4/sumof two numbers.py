b6920b778c567b125100f6c1edf51fb977ba742c
priya <107272239+priya-thv@users.noreply.github.com>
2024-05-02 21:57:47 +0530

    Create sum of two numbers.py

---
+++ sem4/sumof two numbers.py
@@ -0,0 +1,10 @@
+# sum of two umbers
+a=input("enter a number: ")
+b=input("enter a 2nd number: ")
+# a nd b are string it will return string value
+sum=a+b
+print(sum)
+
+# we need to convert them explicitly
+total=int(a)+int(b)
+print(total)
